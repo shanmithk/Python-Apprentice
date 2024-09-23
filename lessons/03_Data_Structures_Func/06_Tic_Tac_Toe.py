17e3658a8a42dec24bf4dcd3095db6c574117682
shanmithk <kadhome31@gmail.com>
2024-09-23 23:59:32 +0000

    x

--- lessons/03_Data_Structures_Func/06_Tic_Tac_Toe.py
+++ lessons/03_Data_Structures_Func/06_Tic_Tac_Toe.py
@@ -15,8 +15,8 @@ def check_row(l):
     Returns:
         The winner's token ( x or o ) if there is one, otherwise None
         """
-
-    return None
+    if  (l[0] == l[1]) and (l[1] == l[2]):
+        return (l[0])
 
 def check_win(board):
     """Check if a player has won on a board
@@ -26,8 +26,26 @@ def check_win(board):
     Returns:
         The winner's token ( x or o ) if there is one, otherwise None
     """
-
-    return None
+    if check_row (board[0]):
+        return (board[0][0])
+    
+    if check_row (board[1]):
+        return (board[1][0])
+    if check_row (board[2]):
+        return (board[2][0])
+    
+    if check_row([board[0][0],board[1][0], board[2][0]]):
+        return (board[0][0])
+    if check_row([board[0][1],board[1][1], board[2][1]]):
+        return (board[0][1])
+    if check_row([board[0][2],board[1][2], board[2][2]]):
+        return (board[0][2])
+    
+    if check_row([board[0][0],board[1][1], board[2][2]]):
+        return (board[0][0])
+    if check_row([board[2][0],board[1][1], board[0][2]]):
+        return (board[2][0])
+        
 
 # The following code is the main part of the program. It creates a GUI for the
 # game and handles the game logic. Implement the functions above first, then
