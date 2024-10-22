b1ccddee6bfb2db549205a9bc11ca279a041ef29
shanmithk <kadhome31@gmail.com>
2024-10-22 00:00:54 +0000

    hi

--- projects/hotelmanagement.py
+++ projects/hotelmanagement.py
@@ -10,25 +10,31 @@ track room number(s)
 room_nums = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15.16,17,18,19,20)   
 checked_rooms = list()
 rooms = {}
-num_rooms = input("How many rooms would you like")
-price = int(num_rooms) * 200
-print("That will be $" + str(price))
 def add_people():
-    checked_rooms = ""
     name_people = input("What's your name?")
-    room_got = input("Which rooms would you like? The available rooms are" )
+    room_got = input(f"Which rooms would you like? The rooms are{room_nums} and the checked out rooms are{checked_rooms}." )
     booked = room_got.split(",")
+    price = int(len(booked)) * 200
+    print("That will be $" + str(price))
+    checked_rooms.extend([int(room)for room in booked])
     for room in booked:
         rooms[int(room)] = name_people
-            
- 
+def remove_people():
+    name_people = input("What's your name?")
+    for room, name in rooms.items():
+        if name == name_people:
+            del rooms[room]
+   
+
+
          
             
 
 
-add_people()
-print(rooms)
-print(room_nums)
+while room_nums:
+    add_people()
+    print(rooms)
+    print(room_nums)
 
 
 
